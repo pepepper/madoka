dd42ca9d8c581b5ce09a4592cf0e1e2821259e35
reiden <hollyholly2014@outlook.jp>
2019-08-08 23:44:40 +0900

    modified:   tooter.cpp

--- tooter.cpp
+++ tooter.cpp
@@ -5,16 +5,16 @@
 #include <mastodon-cpp/mastodon-cpp.hpp>
 
 int main(int argc, char *argv[]){
-        if(argc<2){
-                std::cout<<"too few argument";
-                return 1;
-        }
-        std:ifstream file("token.txt");
-        std:string domain,token;
-        file>>domain;
-        file>>token;
-        file.close();
-        Mastodon::API masto(domain, token);
-        masto.post(Mastodon::API::v1::statuses,{{"statuses",{args[1]}},{"visibility",{"unlisted"}}});
+	if(argc<2){
+		std::cout<<"too few argument";
+		return 1;
+	}
+	std::ifstream file("token.txt");
+	std::string domain,token;
+	file>>domain;
+	file>>token;
+	file.close();
+	Mastodon::API masto(domain, token);
+	masto.post(Mastodon::API::v1::statuses,{{"statuses",{argv[1]}},{"visibility",{"unlisted"}}});
 }
 
